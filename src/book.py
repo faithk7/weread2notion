25c89d3b52e2eb6f37a50ce91fab2b3ac522d661
faithk7u <faithk7u@outlook.com>
2025-05-05 15:07:48 +0800

    [style] Group book dataclass property

--- src/book.py
+++ src/book.py
@@ -16,10 +16,12 @@ class Book:
     sort: int
     isbn: str = field(default="")
     rating: float = field(default=0.0)
+
     status: str = field(default="")
     reading_time: int = field(default=0)
     finished_date: Optional[int] = field(default=None)
     category: str = field(default="")
+
     bookmark_list: List[Dict] = field(default_factory=list)
     summary: List[Dict] = field(default_factory=list)
     reviews: List[Dict] = field(default_factory=list)
